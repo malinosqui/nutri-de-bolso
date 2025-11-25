e462a996addaaae91fab526112cd20ee17f55626
Gabriel Malinosqui <gabriel@kodus.io>
2025-11-25 20:16:57 -0300

    Initial commit

--- src/database/client.ts
+++
@@ -1,4 +0,0 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
-import { env } from "../config/env.js";
-
-export const supabase: SupabaseClient = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
